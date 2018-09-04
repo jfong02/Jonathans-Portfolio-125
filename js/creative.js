4bdac07aec11a0c4fd8d4dc428ec6939ac1bc52a
jfong02 <jfong02@users.noreply.github.com>
2018-09-03 18:03:19 -0700

    Project buttons don't go to top when pressed again. Now says "collapse" and link changed

--- js/creative.js
+++ js/creative.js
@@ -56,81 +56,113 @@
     distance: '0px'
   }, 300);
 
-  // Magnific popup calls
-  // $('.popup-gallery').magnificPopup({
-  //   delegate: 'a',
-  //   type: 'image',
-  //   tLoading: 'Loading image #%curr%...',
-  //   mainClass: 'mfp-img-mobile',
-  //   gallery: {
-  //     enabled: true,
-  //     navigateByImgClick: true,
-  //     preload: [0, 1]
-  //   },
-  //   image: {
-  //     tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-  //   }
-  // });
-
 })(jQuery); // End of use strict
 
+// Toggle show/hide and go to projects (change project button href to appropriate link)
 function projecttoggleA() {
   var a = document.getElementById("ProjectA");
   var b = document.getElementById("ProjectB");
   var c = document.getElementById("ProjectC");
+  var ba = document.getElementById("projbtnA");
+  var bb = document.getElementById("projbtnB");
+  var bc = document.getElementById("projbtnC");
     b.style.display="none";
     c.style.display="none";
     if (a.style.display === "none") {
         a.style.display = "block";
+        ba.setAttribute("href", "#ProjectA");
+        ba.textContent="Collapse";
+        bb.textContent="Check it Out!"
+        bc.textContent="Check it Out!"
     } else {
         a.style.display = "none";
+        ba.setAttribute("href", "#Project1");
+        ba.textContent="Check it Out!"
     }
-}
-function projectnavA() {
-  var a = document.getElementById("ProjectA");
-  var b = document.getElementById("ProjectB");
-  var c = document.getElementById("ProjectC");
-    a.style.display="block";
-    b.style.display="none";
-    c.style.display="none";
 }
 function projecttoggleB() {
   var a = document.getElementById("ProjectA");
   var b = document.getElementById("ProjectB");
   var c = document.getElementById("ProjectC");
+  var ba = document.getElementById("projbtnA");
+  var bb = document.getElementById("projbtnB");
+  var bc = document.getElementById("projbtnC");
     a.style.display="none";
     c.style.display="none";
     if (b.style.display === "none") {
         b.style.display = "block";
+        bb.setAttribute("href", "#ProjectB");
+        bb.textContent="Collapse";
+        ba.textContent="Check it Out!"
+        bc.textContent="Check it Out!"
     } else {
         b.style.display = "none";
+        bb.setAttribute("href", "#Project2");
+        bb.textContent="Check it Out!"
     }
-}
-function projectnavB() {
-  var a = document.getElementById("ProjectA");
-  var b = document.getElementById("ProjectB");
-  var c = document.getElementById("ProjectC");
-    a.style.display="none";
-    b.style.display="block";
-    c.style.display="none";
 }
 function projecttoggleC() {
   var a = document.getElementById("ProjectA");
   var b = document.getElementById("ProjectB");
   var c = document.getElementById("ProjectC");
+  var ba = document.getElementById("projbtnA");
+  var bb = document.getElementById("projbtnB");
+  var bc = document.getElementById("projbtnC");
     a.style.display="none";
     b.style.display="none";
     if (c.style.display === "none") {
         c.style.display = "block";
+        bc.setAttribute("href", "#ProjectC");
+        bc.textContent="Collapse";
+        ba.textContent="Check it Out!"
+        bb.textContent="Check it Out!"
     } else {
         c.style.display = "none";
+        bc.setAttribute("href", "#Project3");
+        bc.textContent="Check it Out!"
     }
+}
+
+//For projects via navbar
+function projectnavA() {
+  var a = document.getElementById("ProjectA");
+  var b = document.getElementById("ProjectB");
+  var c = document.getElementById("ProjectC");
+  var ba = document.getElementById("projbtnA");
+  var bb = document.getElementById("projbtnB");
+  var bc = document.getElementById("projbtnC");
+    a.style.display="block";
+    b.style.display="none";
+    c.style.display="none";
+    ba.textContent="Collapse"
+    bb.textContent="Check it Out!"
+    bc.textContent="Check it Out!"
+}
+function projectnavB() {
+  var a = document.getElementById("ProjectA");
+  var b = document.getElementById("ProjectB");
+  var c = document.getElementById("ProjectC");
+  var ba = document.getElementById("projbtnA");
+  var bb = document.getElementById("projbtnB");
+  var bc = document.getElementById("projbtnC");
+    a.style.display="none";
+    b.style.display="block";
+    c.style.display="none";
+    ba.textContent="Collapse"
+    bb.textContent="Check it Out!"
+    bc.textContent="Check it Out!"
 }
 function projectnavC() {
   var a = document.getElementById("ProjectA");
   var b = document.getElementById("ProjectB");
   var c = document.getElementById("ProjectC");
+  var ba = document.getElementById("projbtnA");
+  var bb = document.getElementById("projbtnB");
+  var bc = document.getElementById("projbtnC");
     a.style.display="none";
     b.style.display="none";
     c.style.display="block";
+    ba.textContent="Collapse"
+    bb.textContent="Check it Out!"
+    bc.textContent="Check it Out!"
 }
